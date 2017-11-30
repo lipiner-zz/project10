1da6dd271fd26c0b6afc1d547a65d359b71655d0
lipiner <ylipiner@gmail.com>
2017-11-30 19:27:53 +0200

    New project! :)

--- JackTokenizer.py
+++ JackTokenizer.py
@@ -1,10 +1,24 @@
+KEYWORD_TYPE = "keyword"
+SYMBOL_TYPE = "symbol"
+INTEGER_CONST_TYPE = "integerConstant"
+STRING_COST_TYPE = "StringConstant"
+IDENTIFIER = "identifier"
+EOF_NOTE = ''
+
+
 class JackTokenizer:
 
-    def __init__(self, input_file):
-        pass
+    def __init__(self, input_stream):
+        self.__file = input_stream
+        self.__current_tokens = []
 
     def has_more_tokens(self):
-        pass
+        if len(self.__current_tokens) == 0:
+            line = self.__file.readline()
+            if line == EOF_NOTE:
+                return False
+        else:
+            return True
 
     def advance(self):
         pass
