c65aa66ad8bbababdb29923e2e68e38cf886f920
lipiner <ylipiner@gmail.com>
2017-11-30 19:12:49 +0200

    New project! :)

---
+++ JackTokenizer.py
@@ -0,0 +1,28 @@
+class JackTokenizer:
+
+    def __init__(self, input_file):
+        pass
+
+    def hasMore_tokens(self):
+        pass
+
+    def advance(self):
+        pass
+
+    def token_type(self):
+        pass
+
+    def key_word(self):
+        pass
+
+    def symbol(self):
+        pass
+
+    def identifier(self):
+        pass
+
+    def int_val(self):
+        pass
+
+    def string_val(self):
+        pass
