bef1aca7fb1886b64c87f47db160f6de16582a24
lipiner <ylipiner@gmail.com>
2017-11-30 19:04:18 +0200

    New project! :)

---
+++ CompilationEngine.py
@@ -0,0 +1,56 @@
+class CompilationEngine:
+
+    def compile_class(self):
+        pass
+
+    def compile_class_var_dec(self):
+        pass
+
+    def compile_subroutine(self):
+        pass
+
+    def compile_parameter_list(self):
+        pass
+
+    def compile_var_dec(self):
+        pass
+
+    def compile_statements(self):
+        pass
+
+    def compile_do(self):
+        pass
+
+    def compile_let(self):
+        pass
+
+    def compile_while(self):
+        pass
+
+    def compile_return(self):
+        pass
+
+    def compile_if(self):
+        pass
+
+    def compile_expression(self):
+        pass
+
+    def compile_term(self):
+        pass
+
+    def compile_expression_list(self):
+        pass
+
+    def check_keyword(self, keyword_list):
+        pass
+
+    def check_type(self):
+        pass
+
+    def check_op(self):
+        pass
+
+    def check_unary_op(self):
+        pass
+
